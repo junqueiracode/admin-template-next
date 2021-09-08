02829f1f7a99f22a8c625adc0d65cc839b6fd069
Ale Junqueira <alessandro.junqueira@intergalaxy.io>
2021-09-08 16:02:56 -0300

    up

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className={
+      `flex justify-center items-center h-screen
+        bg-gradient-to-r from-gray-200 to-blue-400
+      `
+    }>
+      <h1>Admin</h1>
+    </div>
+  )
+}
