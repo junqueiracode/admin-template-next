5505c1a6968f28c85e037823bfb53d743ba664e6
Ale Junqueira <alessandro.junqueira@intergalaxy.io>
2021-09-11 15:07:31 -0300

    up

---
+++ src/components/template/Layout.tsx
@@ -0,0 +1,12 @@
+interface LayoutProps {
+    titulo: string
+    subtitulo: string
+    children?: any
+}
+
+
+export default function Layout(props: LayoutProps) {
+    return (
+        <div></div>
+    )
+}
